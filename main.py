c2301a2ea1e112f33637d128b6d51a06304fec17
ryosyntaxx <riodwi12174@gmail.com>
2024-01-09 11:36:28 +0700

    prepare completed

---
+++ main.py
@@ -0,0 +1,6 @@
+
+from src import Reporters
+
+if __name__ == '__main__':
+    rsf = Reporters()
+    rsf.main()
---
+++ src/service/reporters.py
@@ -0,0 +1,19 @@
+import os
+
+from pyquery import PyQuery
+from time import time
+from datetime import datetime
+
+from src.utils.parser import Parser
+from src.utils.fileIO import File
+from src.utils.logs import logger
+from src.utils.corrector import vname
+
+class Reporters:
+    def __init__(self) -> None:
+        ...
+
+
+    def main(self):
+        ...
+
